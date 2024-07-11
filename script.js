a0145975f64cff4d3b3bc66d7f13b6466e638fac
ta.holme <ta.holme@soprasteria.com>
2024-07-11 10:42:41 +0200

    altered logic to support higher numbered dice

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const rollbtnEl = document.getElementById("rollbtn");
 const getColorValue = () => ["Blue", "Red", "Green"][Math.floor(Math.random() * 3)];
 const getDiceValue = () => Math.ceil(Math.random() * 6);
 const generateDices = (count) => new Array(count).fill().map(() => ({"color": getColorValue(), "value": getDiceValue()}));
-const calculatePetals = (dices) => dices.filter(({value}) => value == 3 || value == 5).reduce((acc, {value}) => acc + value-1,0);
+const calculatePetals = (dices) => dices.filter(({value}) => value % 2 == 1).reduce((acc, {value}) => acc + value-1,0);
 
 
 // Game Logic
