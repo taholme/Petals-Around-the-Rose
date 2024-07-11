fe916cc378be922aaf8ea14d4d12d21d788935ab
ta.holme <ta.holme@soprasteria.com>
2024-07-11 10:40:37 +0200

    added event loop for button

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+// DOM
+const dicesEl = document.getElementById("dices");
+const scoreEl = document.getElementById("petalscore");
+const rollbtnEl = document.getElementById("rollbtn");
+
+// Puzzle Logic
 const getColorValue = () => ["Blue", "Red", "Green"][Math.floor(Math.random() * 3)];
 const getDiceValue = () => Math.ceil(Math.random() * 6);
 const generateDices = (count) => new Array(count).fill().map(() => ({"color": getColorValue(), "value": getDiceValue()}));
 const calculatePetals = (dices) => dices.filter(({value}) => value == 3 || value == 5).reduce((acc, {value}) => acc + value-1,0);
 
 
-let dices = generateDices(5);
+// Game Logic
+function setup(count) {
+    let dices = generateDices(5);
+    let petals = calculatePetals(dices);
 
-let petals = calculatePetals(dices);
+    dicesEl.innerHTML = dices.map((dice) => `<img class="dice" src="./src/${dice.color}${dice.value}.jpg">`).join('');
+    scoreEl.innerText = petals;
+}
+
+rollbtnEl.addEventListener('click', setup);
+setup();
